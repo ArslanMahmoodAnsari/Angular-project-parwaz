dd62127ed5d410cc5b8a62344234403b3f87f33d
ArslanAnsari-SystemsLTD <arslan.ansari@systemsltd.com>
2021-12-11 12:44:14 +0500

    feat: sidebar dynamic

---
+++ src/app/modals/sidebar.ts
@@ -0,0 +1,7 @@
+import { IconDefinition } from '@fortawesome/fontawesome-svg-core';
+
+export interface ISideBar {
+	icon: IconDefinition;
+	title: string;
+	isHeader: boolean;
+}
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,12 +1,57 @@
 import { Component, OnInit } from '@angular/core';
-import { faBell } from '@fortawesome/free-solid-svg-icons';
+import {
+	faUserCircle,
+	faTachometerAlt,
+	faUser,
+	faSignOutAlt,
+	faStickyNote,
+} from '@fortawesome/free-solid-svg-icons';
+import { ISideBar } from '../modals/sidebar';
+
 @Component({
 	selector: 'app-sidebar',
 	templateUrl: './sidebar.component.html',
 	styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent implements OnInit {
-	faBell = faBell;
+	faUserCircle = faUserCircle;
+	sideBarItems: ISideBar[] = [
+		{
+			icon: faTachometerAlt,
+			title: 'APP',
+			isHeader: true,
+		},
+		{
+			icon: faTachometerAlt,
+			title: 'Dashboard',
+			isHeader: false,
+		},
+		{
+			icon: faUser,
+			title: 'User',
+			isHeader: false,
+		},
+		{
+			icon: faStickyNote,
+			title: 'post',
+			isHeader: false,
+		},
+		{
+			icon: faTachometerAlt,
+			title: 'Account',
+			isHeader: true,
+		},
+		{
+			icon: faUser,
+			title: 'My Account',
+			isHeader: false,
+		},
+		{
+			icon: faSignOutAlt,
+			title: 'Logout',
+			isHeader: false,
+		},
+	];
 
 	constructor() {}
 
